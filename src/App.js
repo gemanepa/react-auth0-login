6fc1d67aa87a77702dc9936031568fc409b86328
gemanepa <contacto@gemanepa.com>
2019-11-05 13:08:14 -0300

    Connected and conditional rendering dog and cat apis through SWR

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import { useAuth0 } from "./react-auth0-spa";
 import Profile from "./components/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import NavBar from "./components/NavBar";
-
+import RandomCuteAnimal from './components/RandomCuteAnimal';
 import './App.css';
 
 function App() {
-  const { loading } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, loading } = useAuth0();
+  const dog = {
+    type: 'dog',
+    url:  'https://dog.ceo/api/breeds/image/random'
+  }
+  const cat = {
+    type: 'cat',
+    url:  'https://api.thecatapi.com/v1/images/search?size=med'
+  }
 
   if (loading) {
     return (
@@ -20,11 +28,30 @@ function App() {
 
       {/* New - use BrowserRouter to provide access to /profile */}
       <BrowserRouter>
-        <NavBar />
+        <NavBar auth0={{isAuthenticated, loginWithRedirect, logout}}/>
         <header>
           <h1 style={{textAlign: 'center'}}>Login Template</h1>
           <h2 style={{textAlign: 'center'}}>Using React Hooks + SWR + Auth0</h2>
         </header>
+
+        {!isAuthenticated && (
+        <>
+          {/* <RandomCuteAnimal cuteAnimal={dog}/> */}
+          <button
+            onClick={() =>
+              loginWithRedirect({})
+            }
+            style={{width: '10%', height: '100%'}}
+          >
+            Log in
+          </button>
+        </>)}
+
+        {isAuthenticated && (
+        <>
+        {/* <RandomCuteAnimal cuteAnimal={cat}/> */}
+        <button onClick={() => logout()}>Log out</button>
+        </>)}
 
         <Switch>
           <Route path="/" exact />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
 import logo from '../logo.png';
-const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+const NavBar = (props) => {
+  const { isAuthenticated, loginWithRedirect, logout } = props.auth0;
 
   return (
     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
       <a style={{width: 'auto', height:'auto'}} href="https://gemanepa.com">
-        <img src={logo} title="gemanepa.com" alt="" style={{width: '150px', height: 'auto'}}/>
+        <img src={logo} title="gemanepa.com" alt="" style={{width: '150px', height: 'auto', opacity: 0}}/>
       </a>
       {!isAuthenticated && (
         <button
           onClick={() =>
             loginWithRedirect({})
           }
+          style={{width: 'auto', height: 'auto', backgroundColor: 'transparent', border: '0px', color: 'darkred', fontSize: '150%', cursor: 'pointer'}}
         >
           Log in
         </button>
